0cc1f57c6af5ac16a4a8037b463454cf8a09c43a
bitpixeled <bitpixeled@gmail.com>
2021-04-29 17:41:43 +0300

    ++

---
+++ src/main/java/com/halils/weathertoday/service/WeatherService.java
@@ -0,0 +1,14 @@
+package com.halils.weathertoday.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+}
