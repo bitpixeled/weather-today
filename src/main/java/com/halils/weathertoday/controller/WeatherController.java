853761c69f3797cd36f24756064a3142e4e9cf34
bitpixeled <bitpixeled@gmail.com>
2021-05-05 23:48:10 +0300

    ++

--- src/main/java/com/halils/weathertoday/controller/WeatherController.java
+++ src/main/java/com/halils/weathertoday/controller/WeatherController.java
@@ -1,8 +1,21 @@
 package com.halils.weathertoday.controller;
 
+import com.halils.weathertoday.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping(value = "/")
+    public ResponseEntity<String> getWeather (){
+        return weatherService.getWeather();
+    }
+
 
 }
--- src/main/java/com/halils/weathertoday/service/WeatherService.java
+++ src/main/java/com/halils/weathertoday/service/WeatherService.java
@@ -1,14 +1,21 @@
 package com.halils.weathertoday.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 public class WeatherService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    public ResponseEntity<String> getWeather (){
+        return ResponseEntity.of(Optional.of("ops"));
+    }
 
 
 
